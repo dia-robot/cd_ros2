9ea85c98f4c328a1a3b17b567793bd6dd53a1f6d
shaun <da5668@srmist.edu.in>
2025-09-03 10:22:25 +0530

    Initial commit of ros2_ws

---
+++ src/robot_controller_sept_03_cd/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+package_name = 'robot_controller_sept_03_cd'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='admin-1',
+    maintainer_email='da5668@srmist.edu.in',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
